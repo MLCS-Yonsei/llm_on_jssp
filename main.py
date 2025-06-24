fc57d45d4120e5dde7ecea1a8b38d944a912f280
Ironsesameoil <127085535+Ironsesameoil@users.noreply.github.com>
2025-06-25 02:07:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,15 +2,15 @@ from llm_module import llm_generate_final_output
 from optimizer import solve_schedule
 import json
 from prompt_entrance import make_input_matrix
+import os
+import config
 
-### Path to 'llm_on_jssp' folder ###
-path = #"File path of 'llm_on_jssp'/" 
-#################################### 
 
 def convert_solution_for_llm(solution_json):
     sol = solution_json['solution']
     makespan = sol['makespan']
     filtered_schedule = []
+
     for job in sol['schedule']:
         filtered_job = [op for op in job if not (isinstance(op[0], int) and str(op[0]).startswith('9'))] 
         filtered_schedule.append(filtered_job)
@@ -31,8 +31,12 @@ def extract_answer(llm_output):
 
 if __name__ == '__main__':
     
-    prompt = path + "input/problem_prompt.json"             
-    env_image = path + "input/env_img.png"      
+    #prompt = path + "/input/problem_prompt.json"             
+    #env_image = path + "/input/env_img.png"      
+
+    path = config.to_project_path
+    prompt = config.path_to_input_prompt
+    env_image = config.path_to_input_image
 
     # Step 1: LLM processes prompt + image to structured JSON
     problem_json = make_input_matrix(prompt, env_image, al_path=path)
@@ -42,7 +46,7 @@ if __name__ == '__main__':
 
     # Step 3: LLM evaluates and generates final prompt-like output
     llm_prompt = convert_solution_for_llm(solution_json)
-    final_output = llm_generate_final_output(llm_prompt, al_path=path)
+    final_output = llm_generate_final_output(llm_prompt)
 
     # Step 4: Final parsing
     final_answer = extract_answer(final_output)
